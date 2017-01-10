c93bfd8d40fb4cd0e4d54a66ba686b9385a27b8c
sandeep <sandeep@Sandeep>
2017-01-10 13:14:04 +0000

    test base updated and error removed

--- actiTime/src/actiTime/testBase/TestBase.java
+++ actiTime/src/actiTime/testBase/TestBase.java
@@ -5,9 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.server.handler.ImplicitlyWait;
-
 import actiTime.testUtils.Utills;
 
 public class TestBase extends Utills{
@@ -50,13 +49,42 @@ public class TestBase extends Utills{
 		Repository.load(FI);
 		
 	}
-	public static WebElement getLocator(String locator){
+	public static WebElement getLocator(String locator) throws Exception{
 		String locatorType = locator.split("_")[0];
 		String locatorValue = locator.split("_")[1];
 		if (locatorType.toLowerCase().equals("id"))
-		return driver
+		return driver.findElement(By.id(locatorValue));
+		else if (locatorType.toLowerCase().equals("name"))
+			return driver.findElement(By.name(locatorType));
+		else if( (locatorType.toLowerCase().equals("classname"))
+			||(locatorType.toLowerCase().equals("class")))
+			return driver.findElement(By.className(locatorType));
 		
+		else if ((locatorType.toLowerCase().equals("tagname"))
+			||(locatorType.toLowerCase().equals("tag")))
+			return driver.findElement(By.tagName(locatorType));
+		
+		else if (locatorType.toLowerCase().equals("linktext")
+			||(locatorType.toLowerCase().equals("link")))
+			return driver.findElement(By.linkText(locatorType));
+		
+		else if (locatorType.toLowerCase().equals("partiallinktext"))
+			return driver.findElement(By.partialLinkText(locatorType));
+		
+		else if( (locatorType.toLowerCase().equals("cssselector"))
+			||(locatorType.toLowerCase().equals("css")))
+			return driver.findElement(By.cssSelector(locatorType));
+		
+		else if (locatorType.toLowerCase().equals("xpath"))
+			return driver.findElement(By.xpath(locatorType));
+		else 
+			throw new Exception("Unknow eliment locator type " + locatorType);		
+	}
+	public WebElement geWebElement(String locator) throws Exception{
+	return getLocator(Repository.getProperty(locator));
 	}
 	
-	
+	public void closeBrowser(){
+		driver.quit();
+	}
 }
